9adbbc06df6231e05083e896db22aa7f4711ea62
alex <alexandros441@yahoo.gr>
2021-02-13 15:21:23 +0200

    init: start project

---
+++ src/client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss">
+  $font-dir: '../src/assets/fonts/';
+
+  @font-face {
+    font-family: 'Now';
+    src: url('#{$font-dir}/now/Now-Regular.otf');
+    src: url('#{$font-dir}/now/Now-Bold.otf');
+    src: url('#{$font-dir}/now/Now-Light.otf');
+    src: url('#{$font-dir}/now/Now-Thin.otf');
+    src: url('#{$font-dir}/now/Now-Medium.otf');
+  }
+
+  #app {
+    font-family: 'Now' !important;
+  }
+</style>
